ecd6e01342b39c5cee55565d8ccfcf8d57809c91
Thomaz França <thomazf@ciandt.com>
2022-07-15 16:25:36 -0300

    Padrões de projeto Java - Continuação

--- 0x01/salario/CalculadorSalarioService.java
+++ 0x01/salario/CalculadorSalarioService.java
@@ -1,29 +1,21 @@
-package salario;
-
 import java.util.UUID;
 
 public class CalculadorSalarioService {
-    public UUID uuid = UUID.randomUUID();
-
     private static CalculadorSalarioService instancia;
 
-    private CalculadorSalarioService() {
-    }
+    public UUID uuid = UUID.randomUUID();
+
+    private CalculadorSalarioService() {}
 
     public static CalculadorSalarioService getCalculadorSalarioService() {
-        if(instancia == null) {
+        if (instancia == null) {
             instancia = new CalculadorSalarioService();
-            return instancia;
-        } else {
-            return instancia;
         }
+        return instancia;
     }
 
-    public double calcularSalarioLiquido(double salarioBruto, double valorDescontos, double valorVendas, double percentualComissao) {
-        return salarioBruto - valorDescontos + (valorVendas * (percentualComissao / 100));
-    }
-
-    public UUID getUuid() {
-        return uuid;
+    public double calcularSalarioLiquido(
+            double salarioBruto, double valorDescontos, double valorVendas, double percentualComissao) {
+        return (salarioBruto - valorDescontos) + (valorVendas * (percentualComissao / 100));
     }
 }
