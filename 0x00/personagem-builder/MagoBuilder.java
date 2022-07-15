3c287b47880d6f2a0f50a427e1b1d3807830c862
Thomaz França <thomazf@ciandt.com>
2022-07-15 08:24:03 -0300

    Padrões de projeto Java - Introdução

---
+++ 0x00/personagem-builder/MagoBuilder.java
@@ -0,0 +1,50 @@
+public class MagoBuilder implements Builder {
+
+    private String nome;
+    private int forca;
+    private TipoPersonagem tipo;
+    private int inteligencia;
+    private int vigor;
+    private int resistencia;
+    private int destreza;
+
+
+    @Override
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    @Override
+    public void setForca(int forca) {
+        this.forca = forca;
+    }
+
+    @Override
+    public void setTipo(TipoPersonagem tipo) {
+        this.tipo = tipo;
+    }
+
+
+    @Override
+    public void setInteligencia(int inteligencia) {
+        this.inteligencia = inteligencia;
+    }
+
+    @Override
+    public void setVigor(int vigor) {
+        this.vigor = vigor;
+    }
+
+    @Override
+    public void setResistencia(int resistencia) {
+        this.resistencia = resistencia;
+    }
+
+    @Override
+    public void setDestreza(int destreza) {
+        this.destreza = destreza;
+    }
+    public Mago build(){
+        return new Mago(nome,  inteligencia, forca, vigor, resistencia, destreza);
+    }
+}
