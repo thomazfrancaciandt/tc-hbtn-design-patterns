2ad34bf6c93ee6a31e183cccfd40783a659b3e19
Thomaz França <thomazf@ciandt.com>
2022-07-15 17:15:57 -0300

    Padrões de projeto Java - Continuação

--- 0x01/salario/CalculadorSalarioService.java
+++ 0x01/salario/CalculadorSalarioService.java
@@ -10,10 +10,8 @@ public class CalculadorSalarioService {
     public static CalculadorSalarioService getCalculadorSalarioService(){
         if(instacia == null){
             instacia = new CalculadorSalarioService();
-            return instacia;
-        }else {
-            return instacia;
         }
+        return instacia;
     }
     public double calcularSalarioLiquido(double salarioBruto, double valorDescontos, double valorVendas, double percentualComissao){
         return salarioBruto - valorDescontos + (valorVendas * (percentualComissao / 100));
