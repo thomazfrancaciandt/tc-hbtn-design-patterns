de0a8b18cbde874d79815b548143f8a175a705ad
Thomaz França <thomazf@ciandt.com>
2022-07-15 16:26:30 -0300

    Padrões de projeto Java - Continuação

--- 0x01/salario/CalculadorSalarioService.java
+++ 0x01/salario/CalculadorSalarioService.java
@@ -1,7 +1,7 @@
 import java.util.UUID;
 
 public class CalculadorSalarioService {
-    private static CalculadorSalarioService instancia;
+    public static CalculadorSalarioService instancia;
 
     public UUID uuid = UUID.randomUUID();
 
