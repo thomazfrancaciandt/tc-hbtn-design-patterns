8c7d11a7c42a30c5bfc30c286bab7d572277d4ae
Thomaz França <thomazf@ciandt.com>
2022-07-17 11:15:42 -0300

    Padrões de projeto Java na prática - Parte 1

--- 0x02/lanchonete/Pedido.java
+++ 0x02/lanchonete/Pedido.java
@@ -1,50 +1,34 @@
 import java.util.HashSet;
 
 public class Pedido {
+    private HashSet<ItemPedido> itensDentroCaixa;
+    private HashSet<ItemPedido> itensForaCaixa;
 
-    private HashSet<ItemPedido> itensDentroCaixa = new HashSet<>();
-    private HashSet<ItemPedido> itensForaCaixa = new HashSet<>();
-
-    public HashSet<ItemPedido> getItensDentroCaixa() {
-        return itensDentroCaixa;
+    public Pedido() {
+        this.itensDentroCaixa = new HashSet<ItemPedido>();
+        this.itensForaCaixa = new HashSet<ItemPedido>();
     }
 
-    public void setItensDentroCaixa(HashSet<ItemPedido> itensDentroCaixa) {
-        this.itensDentroCaixa = itensDentroCaixa;
-    }
-
-    public HashSet<ItemPedido> getItensForaCaixa() {
-        return itensForaCaixa;
-    }
-
-    public void setItensForaCaixa(HashSet<ItemPedido> itensForaCaixa) {
-        this.itensForaCaixa = itensForaCaixa;
-    }
-
-    public void adicionarItemDentroCaixa(ItemPedido item){
-        itensDentroCaixa.add(item);
+    public void adicionarItemDentroCaixa(ItemPedido item) {
+        this.itensDentroCaixa.add(item);
     }
 
     public void adicionarItemForaCaixa(ItemPedido item) {
-        itensDentroCaixa.add(item);
+        this.itensForaCaixa.add(item);
     }
 
     @Override
     public String toString() {
-        String foraDaCaixa = "\tFora da Caixa:\n";
-
-        for (ItemPedido item : itensForaCaixa) {
-            foraDaCaixa += String.format("\t\t- %s %s\n", item.getTipo(), item.getNome());
+        StringBuilder itemForaCx = new StringBuilder();
+        StringBuilder itemDentroCx = new StringBuilder();
+        for (ItemPedido element : this.itensForaCaixa) {
+            itemForaCx.append(
+                    String.format("\t\t- %s %s%n", element.getTipo().name(), element.getNome()));
+        }
+        for (ItemPedido e : this.itensDentroCaixa) {
+            itemDentroCx.append(String.format("\t\t- %s %s%n", e.getTipo().name(), e.getNome()));
         }
 
-        String dentroDaCaixa = "\tDentro da Caixa:\n";
-
-        for (ItemPedido item : itensDentroCaixa) {
-            dentroDaCaixa += String.format("\t\t- %s %s\n", item.getTipo(), item.getNome());
-        }
-
-        return foraDaCaixa + dentroDaCaixa;
+        return String.format("\tFora da Caixa:%n%s\tDentro da Caixa:%n%s", itemForaCx, itemDentroCx);
     }
-
-
 }
